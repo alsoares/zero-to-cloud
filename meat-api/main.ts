cc54f3720298cd01c183789ee222a7b09e5e56b9
andre.soares <andre.soares@zup.com.br>
2020-03-30 23:46:00 -0300

    restify

--- meat-api/main.ts
+++ meat-api/main.ts
@@ -1,1 +1,16 @@
-console.log("main script....");
+import * as restify from 'restify';
+
+const server = restify.createServer({
+    name: 'mead-api',
+    version: '1.0.0'
+});
+
+server.get('/hello', (req, res, next) => {
+    res.json({message: 'Hello'});
+    return next()
+});
+
+
+server.listen(3000, () => {
+    console.log('API rodando na porta 3000')
+});
