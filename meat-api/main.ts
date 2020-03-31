5e02e6afa36e2587c8cf032597a4be7ffe886ce4
andre.soares <andre.soares@zup.com.br>
2020-03-31 00:05:44 -0300

    req, res e next

--- meat-api/main.ts
+++ meat-api/main.ts
@@ -5,10 +5,31 @@ const server = restify.createServer({
     version: '1.0.0'
 });
 
-server.get('/hello', (req, res, next) => {
-    res.json({message: 'Hello'});
-    return next()
-});
+server.use(restify.plugins.queryParser());
+
+server.get('/info', [
+    (req, res, next) => {
+        if(req.userAgent() && req.userAgent().includes('MSIE 7.0')){
+            //res.status(400);
+           // res.json({message: 'Atualize seu browser'});
+            
+           let error: any = new Error();
+           error.statusCode = 400;
+           error.message = 'Atualize seu browser';
+           return next(error);
+        }
+        return next()
+
+    }, (req, res, next) => {
+        res.json({
+            browser: req.userAgent(),
+            method: req.method,
+            url: req.href(),
+            path: req.path(),
+            query: req.query
+        });
+        return next()
+    }]);
 
 
 server.listen(3000, () => {
