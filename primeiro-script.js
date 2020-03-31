7f17042c97453e344fb8780611520df32a2e1ef5
andre.soares <andre.soares@zup.com.br>
2020-03-30 21:49:59 -0300

    modulo personalizado

--- primeiro-script.js
+++ primeiro-script.js
@@ -1,3 +1,5 @@
+const fat = require('./fatorial');
+
 console.log('n-fatorial');
 
 /* console.log(`Executando o script a partir do diretorio ${process.cwd()}`);
@@ -6,14 +8,6 @@ process.on('exit', () =>{
     console.log('script está prestes a terminar');
 }); */
 
-const fatorial = (num) => {
-    if(num === 0)
-        return 1;
-
-    return num * fatorial(num - 1);
-}
-
-
 const num = parseInt(process.argv[2]);
 
-console.log(`O Fatorial de ${num} é igual a ${fatorial(num)}`);
+console.log(`O Fatorial de ${num} é igual a ${fat.fatorial(num)}`);
